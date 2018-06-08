e6c043ec1009b06d89478646ac67e8f22fc53fc9
yang <yang-pin@live.com>
2018-06-08 19:41:24 +0200

    can now read in stuff from the json file

--- app/src/main/java/com/example/twitterapp/MainActivity.java
+++ app/src/main/java/com/example/twitterapp/MainActivity.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
     private ListView tweetList;
     private TextView text;
+    private String content;
+    private TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +29,33 @@ public class MainActivity extends AppCompatActivity {
         try{
             byte [] b = new byte[is.available()];
             is.read(b);
-            String fileContent = new String(b);
+            content = new String(b);
         }catch (IOException e){
             e.printStackTrace();
+        }
+
+        tvText = findViewById(R.id.tvText);
+
+        try{
+            JSONObject tweets = new JSONObject(content);
+
+            JSONObject metadata = tweets.getJSONObject("search_metadata");
+            String refresh_url = metadata.getString("refresh_url");
+            tvText.setText(refresh_url);
+
+            JSONArray statuses = tweets.getJSONArray("statuses");
+
+            for (int i = 0; i < statuses.length(); i++){
+                JSONObject status = statuses.getJSONObject(i);
+                String id_str = status.getString("id_str");
+
+                tvText.setText(tvText.getText() + "\n" + id_str);
+            }
+
+
+
+        }catch(JSONException e){
+
         }
     }
 }
