f20c7869ab73d6ac08331badeed8100ebe8325fa
Kyle <kylecunnison@gmail.com>
2018-06-08 17:22:47 +0200

    Added Classes From diagram

--- app/src/main/java/com/example/twitterapp/MainActivity.java
+++ app/src/main/java/com/example/twitterapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.twitterapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
